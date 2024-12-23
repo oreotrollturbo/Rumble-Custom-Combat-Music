ae6277d0cabb438d0cf0c71e9a0d78fd18635a9f
oreotrollturbo <oreotrollturbo@gmail.com>
2024-12-23 03:03:42 +0100

    progress

--- Class1.cs
+++ Class1.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using Il2CppRUMBLE.Networking.MatchFlow;
 using MelonLoader;
-using NAudio.CoreAudioApi;
+using MelonLoader.Utils;
 using NAudio.Wave;
 using RumbleModdingAPI;
+using RumbleModUI;
 using UnityEngine;
 using BuildInfo = CustomBattleMusic.BuildInfo;
 
@@ -17,84 +18,133 @@ namespace CustomBattleMusic
     public static class BuildInfo
     {
         public const string ModName = "Custom battle music";
-        public const string ModVersion = "1.0.1";
+        public const string ModVersion = "1.0.0";
+        public const string Description = "Replace the rumble battle music with your own!";
         public const string Author = "oreotrollturbo";
     }
+    
+    
     public class Main : MelonMod
     {
-        public static WasapiOut currentAudio;
+        private Mod mod = new Mod();
+        
+        public static WaveOutEvent? CurrentAudio;
+        public static string folderPath = MelonEnvironment.UserDataDirectory + @"\CustomBattleMusic";
+        private static string? _currentSceneName;
+        
+        private static ModSetting<float> volume; // max 1f min 0.001f
         public override void OnLateInitializeMelon()
         {
-            string filePath = @"\AdditionalSounds\CustomBattleMusic";
             Calls.onMapInitialized += SceneLoaded;
 
-            if (!File.Exists(filePath))
+            if (!Directory.Exists(folderPath))
             {
-                using (File.Create(filePath))
+                MelonLogger.Error("File at " + folderPath + " does not exist");
+            }
+
+            UI.instance.UI_Initialized += OnUIInit;
+        }
+
+        public void OnUIInit()
+        {
+            mod.ModName = BuildInfo.ModName; // Use the instance-level field
+            mod.ModVersion = BuildInfo.ModVersion;
+            mod.SetFolder("CustomBattleMusic");
+            mod.AddDescription("Description", "", BuildInfo.Description, new Tags { IsSummary = true });
+            
+            volume = mod.AddToList("Float Setting", 0.05f, "Is Float.", new Tags());
+            
+            mod.GetFromFile();
+
+            UI.instance.AddMod(mod); // Use the instance-level field
+            MelonLogger.Msg("Added Mod: " + BuildInfo.ModName);
+        }
+        
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            _currentSceneName = sceneName;
+        }
+        
+        private static void SceneLoaded() // Logic/CombatMusic
+        {
+            if (CurrentAudio != null)
+            {
+                CurrentAudio.Dispose();
+            }
+
+            if (_currentSceneName is "Map0" or "Map1")
+            {
+                MelonLogger.Msg("Map is battle map.");
+                MelonCoroutines.Start(StartBattleMusic(6f)); // Run the coroutine properly
+            }
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(MatchHandler), "ExecuteNextRound")]
+        public static class RoundPatch
+        {
+            public static void Prefix()
+            {
+                try
                 {
-                    MelonLogger.Msg($"File '{filePath}' created successfully.");
+                    if (CurrentAudio != null)
+                    {
+                        CurrentAudio.Dispose();
+                    }
+
+                    var combatMusic = GameObject.Find("CombatMusic");
+                    combatMusic.SetActive(false);
+                    MelonCoroutines.Start(StartBattleMusic(1f)); // Run the coroutine properly
+                }
+                catch (Exception e)
+                {
+                    MelonLogger.Error("Whoops, the music crashed.");
+                    MelonLogger.Error(e.Message);
                 }
             }
         }
 
-
-        private static void SceneLoaded() //  Logic/CombatMusic
-        {
-            currentAudio.Dispose();
-            if (Calls.Scene.GetSceneName() is "Map0" or "Map1")
-            {
-                StartBattleMusic(6f);
-            }
-        }
-        
-        
-        [HarmonyLib.HarmonyPatch(typeof(MatchHandler), "ExecuteNextRound")]
-        public static class RoundPatch //Thanks to Elmish for showing me how to do this
-        {
-            public static void Prefix()
-            {
-                currentAudio.Dispose();
-                var combatMusic = GameObject.Find("CombatMusic");
-                combatMusic.SetActive(false);
-                StartBattleMusic(1f);
-            }
-        }
-        
-        
         private static IEnumerator StartBattleMusic(float delay)
         {
-            
             yield return new WaitForSeconds(delay);
 
-            currentAudio = PlayAudio(@"\AdditionalSounds\jetpack_hellRide.mp3");
+            MelonLogger.Msg("Playing new sound");
+            PlaySoundIfFileExists(Path.Combine(folderPath, "jetpack_hellRide.mp3")); // Use Path.Combine for better path handling
         }
         
-        public static WasapiOut? PlayAudio(string filePath)
+        //Everything bellow was stolen from Ulvak's mod 
+        // https://thunderstore.io/c/rumble/p/UlvakSkillz/Rumble_Additional_Sounds/
+        private static IEnumerator PlaySound(string FilePath)
+        {
+            Mp3FileReader reader = new Mp3FileReader(FilePath);
+            WaveOutEvent waveOut = new WaveOutEvent();
+            CurrentAudio = waveOut;
+            waveOut.Init((IWaveProvider)(object)reader);
+            waveOut.Play();
+            waveOut.Volume = (float)volume.Value; 
+            while ((int)waveOut.PlaybackState == 1)
+            {
+                yield return (object)new WaitForFixedUpdate();
+            }
+            ((Stream)(object)reader).Dispose();
+            waveOut.Dispose();
+        }
+
+        public static void PlaySoundIfFileExists(string soundFilePath)
         {
             try
             {
-                // Create a WaveStream to read the audio file
-                using var audioFileReader = new AudioFileReader(filePath);
-
-                // Initialize WASAPI output device
-                var wasapiOut = new WasapiOut(AudioClientShareMode.Shared, true, 200);
-
-                // Set the input source for playback
-                wasapiOut.Init(audioFileReader);
-
-                // Play the audio
-                wasapiOut.Play();
-
-                Console.WriteLine("Playing audio at " + filePath);
-                
-                wasapiOut.Volume /= 2;
-
-                return wasapiOut;
+                if (File.Exists(soundFilePath))
+                {
+                    MelonCoroutines.Start(PlaySound(soundFilePath)); // Pass the correct path
+                }
+                else
+                {
+                    MelonLogger.Error($"Sound file does not exist: {soundFilePath}");
+                }
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                Console.WriteLine($"An error occurred while playing the file '{filePath}': {ex.Message}");
-                return null;
+                MelonLogger.Error($"Failed to play sound: {e.Message}");
             }
         }
     }
