2f18b43d958759781569aff31a0e40d31cf4d6b8
oreotrollturbo <oreotrollturbo@gmail.com>
2024-12-26 18:05:27 +0100

    Changes

--- Class1.cs
+++ Class1.cs
@@ -9,8 +9,6 @@ using RumbleModUI;
 using UnityEngine;
 using BuildInfo = CustomBattleMusic.BuildInfo;
 
-
-
 [assembly: MelonInfo(typeof(CustomBattleMusic.Main), BuildInfo.ModName, BuildInfo.ModVersion, BuildInfo.Author)]
 [assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
 
@@ -23,15 +21,14 @@ namespace CustomBattleMusic
         public const string Description = "Replace the rumble battle music with your own!";
         public const string Author = "oreotrollturbo";
     }
-    
-    
+
     public class Main : MelonMod
     {
         private Mod mod = new Mod();
-        
+
         public static WaveOutEvent? CurrentAudio;
         public static string folderPath = MelonEnvironment.UserDataDirectory + @"\CustomBattleMusic";
-        
+
         private static ModSetting<float> volume; // max 1f min 0.001f
         private static ModSetting<bool> isModEnabled;
         public override void OnLateInitializeMelon()
@@ -52,12 +49,12 @@ namespace CustomBattleMusic
             mod.ModVersion = BuildInfo.ModVersion;
             mod.SetFolder("CustomBattleMusic");
             mod.AddDescription("Description", "", BuildInfo.Description, new Tags { IsSummary = true });
-            
+
             volume = mod.AddToList("Volume", 0.05f, "The volume at which every song will play", new Tags());
-            isModEnabled = mod.AddToList("Is mod enabled", true,1, "Enable or disable the mod", new Tags());
-            
+            isModEnabled = mod.AddToList("Is mod enabled", true, 1, "Enable or disable the mod", new Tags());
+
             mod.GetFromFile();
-            
+
             mod.ModSaved += Save;
 
             UI.instance.AddMod(mod); // Use the instance-level field
@@ -66,20 +63,25 @@ namespace CustomBattleMusic
 
         private static void Save()
         {
+            if (CurrentAudio == null) return;
+
             CurrentAudio.Volume = (float)volume.Value;
 
             if ((bool)isModEnabled.Value == false)
             {
-                CurrentAudio.Dispose();
+                CurrentAudio.Stop();
+                CurrentAudio = null; // Always reset
+                MelonLogger.Msg("Mod disabled stopping all music");
             }
         }
-        
+
         private static void SceneLoaded() // Logic/CombatMusic
         {
-            
             if (CurrentAudio != null)
             {
+                MelonLogger.Msg("New scene loaded disposing of old music object");
                 CurrentAudio.Dispose();
+                CurrentAudio = null;
             }
         }
 
@@ -92,17 +94,19 @@ namespace CustomBattleMusic
                 {
                     if (roundNo > 0 && CurrentAudio != null)
                     {
-                        CurrentAudio.Play();
+                        MelonLogger.Msg("New round started resuming music");
+                        PlayBattleMusic(2f);
                     }
-                    
-                    if (CurrentAudio != null)
+                    else if (roundNo == 0)
                     {
-                        CurrentAudio.Dispose();
+                        MelonCoroutines.Start(StartBattleMusic(1f)); // Run the coroutine properly
                     }
 
                     var combatMusic = GameObject.Find("CombatMusic");
-                    combatMusic.SetActive(false);
-                    MelonCoroutines.Start(StartBattleMusic(1f)); // Run the coroutine properly
+                    if (combatMusic != null)
+                    {
+                        combatMusic.SetActive(false);
+                    }
                 }
                 catch (Exception e)
                 {
@@ -113,60 +117,64 @@ namespace CustomBattleMusic
         }
 
         [HarmonyLib.HarmonyPatch(typeof(MatchHandler), "OnPlayerDefeated")]
-        public static class PlayerKill
+        public static class PlayerDefeat
         {
-            public static void Prefix( ref Player p, ref string killDescription)
+            public static void Prefix(Player p, string killDescription)
             {
                 if (!(Calls.Scene.GetSceneName() is "Map0" or "Map1")) return;
-                
-                MelonLogger.Warning("PlayerKill trigerred");
 
                 if (CurrentAudio != null)
                 {
+                    MelonLogger.Msg("Round ended pausing music");
                     CurrentAudio.Pause();
                 }
             }
         }
 
-        
+        private static IEnumerator PlayBattleMusic(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            CurrentAudio?.Play();
+        }
 
         private static IEnumerator StartBattleMusic(float delay)
         {
             if (!(bool)isModEnabled.Value) yield break;
-            
+
             yield return new WaitForSeconds(delay);
 
             MelonLogger.Msg("Playing new song");
-            
+
             string[] mp3Files = Directory.GetFiles(folderPath, "*.mp3");
-            
+
             if (mp3Files.Length == 0)
             {
                 MelonLogger.Warning("No MP3 files found in the specified folder.");
             }
-            
+
             System.Random random = new System.Random();
             int randomIndex = random.Next(mp3Files.Length);
-            
+
             PlaySoundIfFileExists(mp3Files[randomIndex]);
         }
-        
+
         //Everything bellow was stolen from Ulvak's mod 
         // https://thunderstore.io/c/rumble/p/UlvakSkillz/Rumble_Additional_Sounds/
-        private static IEnumerator PlaySound(string FilePath)
+        private static IEnumerator PlaySound(string FilePath) //TODO make the sound loop
         {
             Mp3FileReader reader = new Mp3FileReader(FilePath);
             WaveOutEvent waveOut = new WaveOutEvent();
+            waveOut.Init(reader);
+            waveOut.Volume = (float)volume.Value;
             CurrentAudio = waveOut;
-            waveOut.Init((IWaveProvider)(object)reader);
+
             waveOut.Play();
-            
-            waveOut.Volume = (float)volume.Value; 
-            while ((int)waveOut.PlaybackState == 1)
+
+            while (waveOut.PlaybackState == PlaybackState.Playing)
             {
-                yield return (object)new WaitForFixedUpdate();
+                yield return new WaitForFixedUpdate();
             }
-            ((Stream)(object)reader).Dispose();
+
             waveOut.Dispose();
         }
 
@@ -190,4 +198,3 @@ namespace CustomBattleMusic
         }
     }
 }
-
